fb56f1af73085378f5b66e5e9bf97521cf76e29d
Shayan-meo <159838275+Shayan-meo@users.noreply.github.com>
2026-02-12 02:44:36 +0500

    Add files via upload

---
+++ js-form-console/app.js
@@ -0,0 +1,21 @@
+function onSubmit(nameId, emailId , passId){
+    var Text = document.getElementById(nameId)
+    var Email = document.getElementById(emailId)
+    var Password = document.getElementById(passId)
+
+    // console.log( `${show1} Name ${show2} Email ${show3} Password`);
+    
+    console.log(`Name  ${Text}`);
+    console.log(`Name  ${Text}`);
+    console.log(`Name  ${Text}`);
+  
+
+    Text.value = "";
+    Email.value = "";
+    Password.value="";
+
+    
+}
+
+
+
