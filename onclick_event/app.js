334feceb69f2fbb198adf425f509748e391e28ef
Shayan-meo <159838275+Shayan-meo@users.noreply.github.com>
2026-02-13 02:17:01 +0500

    Expended_Text

---
+++ onclick_event/app.js
@@ -0,0 +1,9 @@
+function expandText(textId) {
+    var expandedText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate! Doloribus, magnam. Voluptas, voluptate! Doloribus, magnam. Voluptas, voluptate! Doloribus, magnam. Voluptas, voluptate! Doloribus, magnam. Voluptas, voluptate! Doloribus, magnam. Voluptas, voluptate! Doloribus, magnam. Voluptas, voluptate! Doloribus, magnam. Voluptas, voluptate! Doloribus magnam. Voluptas, voluptate! Doloribus, magnam. Voluptas, voluptate! Doloribus, magnam.";
+
+    var textElement = document.getElementById(textId);
+
+    // console.log(textElement);
+  
+    textElement.innerText = expandedText;
+}
